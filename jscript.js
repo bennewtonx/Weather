e6e9187cadca08b4bee99deed2cf921a3fd5ff9d
Ben Newton <ben.newton112@gmail.com>
2023-07-01 12:28:12 +0100

    1228

--- jscript.js
+++ jscript.js
@@ -1,32 +1,42 @@
 const apiKey = "30eb778dde414e85e06c8f0f558ecdef";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-const nclWeatherIcon = document.querySelector("#nclWeatherIcon")
 
-async function checkWeather () {
+const searchBar = document.querySelector(".search input")
+const searchBtn = document.querySelector(".search button")
+
+
+async function checkWeather (cityName) {
     const response = await fetch(apiUrl + cityName + `&appid=${apiKey}`);
     var data = await response.json();
 
+    document.querySelector(".cityPlaceholder").innerHTML = cityName
 
-var cityName = document.getElementsByClassName("searchBar").value;
 
-document.querySelector("#newcastle").innerHTML = "Newcastle" + " " + Math.round(data.main.temp) + "°C";
+    if(data.weather[0].main == "Sunny") {
+        mainCityImg.src = "sun emoji.png";
+    }
+    if(data.weather[0].main == "Clouds") {
+        mainCityImg.src = "cloud emoji.png";
+    }
+    else if (data.weather[0].main == "Clear") {
+        mainCityImg.src = "sun emoji.png";
+    }
+    else if (data.weather[0].main == "Rain") {
+        mainCityImg.src = "Rain Cloud.png";
+    }
+    else if (data.weather[0].main == "Drizzle") {
+        mainCityImg.src = "Rain Cloud.png";
+    }
+    else if (data.weather[0].main == "Mist") {
+        mainCityImg.src = "cloud emoji.png";
+    }
 
-if(data.weather[0].main == "Sunny") {
-    nclWeatherIcon.src = "sun emoji.png";
 }
-if(data.weather[0].main == "Clouds") {
-    nclWeatherIcon.src = "cloud emoji.png";
-}
-else if (data.weather[0].main == "Clear") {
-    nclWeatherIcon.src = "sun emoji.png";
-}
-else if (data.weather[0].main == "Rain") {
-    nclWeatherIcon.src = "Rain Cloud.png";
-}
-else if (data.weather[0].main == "Drizzle") {
-    nclWeatherIcon.src = "Rain Cloud.png";
-}
-else if (data.weather[0].main == "Mist") {
-    nclWeatherIcon.src = "cloud emoji.png";
-}
-}
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBar.value);
+
+})
+
+
+
